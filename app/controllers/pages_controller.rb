e1289dd8e54a4869c2001dc6c5cf38fe97e5d3fc
natebuch <nate.buch@gmail.com>
2018-08-27 19:52:50 -0500

    Fixed routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,43 @@
 class PagesController < ApplicationController
 
-	def home
-	end
+def home
+  if params[:team_name]
+    session[:team_name] = params[:team_name]
+    redirect_to landing_path
+  end
+end
 
-	def landing
-		@teams = [{playerName: "Guru", playerText: "Lil' Bish"},
-       {playerName: "Guru1", playerText: "Lil' Bish 1"},
-       {playerName: "Guru2", playerText: "Lil' Bish 2"},
-       {playerName: "Guru", playerText: "Lil' Bish 3"},
-       {playerName: "Guru", playerText: "Lil' Bish 4"},
-       {playerName: "Guru", playerText: "Lil' Bish 5"},
-       {playerName: "Guru", playerText: "Lil' Bish 6"},
-       {playerName: "Guru", playerText: "Lil' Bish 7"},
-       {playerName: "Guru", playerText: "Lil' Bish 8"},
-       {playerName: "Guru", playerText: "Lil' Bish 9"},
-       {playerName: "Guru", playerText: "Lil' Bish 10"},
-       {playerName: "Guru", playerText: "Lil' Bish 11"}  
-		]
-   
-    
-    @bleh = @teams.select {|x| x[:playerName].downcase == session[:team_name].downcase}
+def landing
+    @teams = [{playerName: "Guru", playerText: "Lil' Bish"},
+    {playerName: "Guru1", playerText: "Lil' Bish 1"},
+    {playerName: "Guru2", playerText: "Lil' Bish 2"},
+    {playerName: "Guru", playerText: "Lil' Bish 3"},
+    {playerName: "Guru", playerText: "Lil' Bish 4"},
+    {playerName: "Guru", playerText: "Lil' Bish 5"},
+    {playerName: "Guru", playerText: "Lil' Bish 6"},
+    {playerName: "Guru", playerText: "Lil' Bish 7"},
+    {playerName: "Guru", playerText: "Lil' Bish 8"},
+    {playerName: "Guru", playerText: "Lil' Bish 9"},
+    {playerName: "Guru", playerText: "Lil' Bish 10"},
+    {playerName: "Guru", playerText: "Lil' Bish 11"}  
+    ]
 
-    if @bleh == []
-    	redirect_to oops_path
-    else
-    	@checkName = session[:team_name]
+  @teams.each do |team|
+    if team[:playerName] == session[:team_name]
+      @team = team
     end
+  end
 
-	end
+  if @team.nil?
+    redirect_to oops_path
+  end
+end
 
-	def search_team
-		session[:team_name] = params[:team_name]	
-		redirect_to landing_path
-	end
+  def search_team
+  
+  end
 
-	def oops
+  def oops
   end
 
 end
