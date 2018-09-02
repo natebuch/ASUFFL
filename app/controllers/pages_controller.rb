f167542d6faa3f9ca3fb7aa4534e3039d4ddeeb0
natebuch <nate.buch@gmail.com>
2018-09-01 23:52:00 -0500

    Change jquery call to HTTPS '

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -58,18 +58,7 @@ def landing
 end
 
 def haha
-      @teams = [{inputName: "the guru", playerName: "The Guru", playerText: "Lil' Bish 1", image: 1},
-    {inputName: "dirty landry", playerName: "Dirty Landry", playerText: "Lil' Bish 2", image:2},
-    {inputName: "the curse is real", playerName: "THe Curse is Real", playerText: "Lil' Bish 3", image: 3},
-    {inputName: "yahoo", playerName: "Yahoo", playerText: "Lil' Bish 4", image: 4},
-    {inputName: "butker munchers", playerName: "butkermunchers", playerText: "Lil' Bish 5", image: 5},
-    {inputName: "ugg life ii", playerName: "UGG Life II", playerText: "Lil' Bish 6", image: 6},
-    {inputName: "ask me about my goat", playerName: "Ask me about my 🐐🤢", playerText: "Lil' Bish 7", image: 7},
-    {inputName: "bernmanual sanders", playerName: "Bernmanual Sanders", playerText: "Lil' Bish 8", image: 8},
-    {inputName: "juju fruits", playerName: "JuJu Fruits", playerText: "Lil' Bish 9", image: 9},
-    {inputName: "rogersville djokovic", playerName: "Rogersville Djokovic", playerText: "Lil' Bish 10", image: 10},
-    {inputName: "the gurley gates", playerName: "The Gurley Gates", playerText: "Lil' Bish 11", image: 11}  
-    ]
+   
 
   @teams.each do |team|
     if team[:inputName] == session[:team_name].downcase
